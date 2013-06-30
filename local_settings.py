b03813ff54f6acebfdb6e3ab4549b75d042c77b7
Tom Carter <user@example.com>
2013-06-30 11:35:54 -0400

    c

--- local_settings.py
+++ local_settings.py
@@ -4,16 +4,16 @@ Configuration Settings
 
 ''' Uncomment to configure using the file.  
 WARNING: Be careful not to post your account credentials on GitHub.
-
-TWILIO_ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
-TWILIO_AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
-TWILIO_APP_SID = "APzzzzzzzzz"
-TWILIO_CALLER_ID = "+17778889999"
 '''
+TWILIO_ACCOUNT_SID = "AC91c14710bfc7a636abbd59396f5ebf14" 
+TWILIO_AUTH_TOKEN = "eef11eaefc5c8ba7b4601082d2e1b978"
+TWILIO_APP_SID = "AP0a10bc90245d454e9f307a5affff7b45"
+TWILIO_CALLER_ID = "+13479604143"
+
 
 # Begin Heroku configuration - configured through environment variables.
-import os
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
-TWILIO_CALLER_ID = os.environ.get('TWILIO_CALLER_ID', None)
-TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID', None)
+#import os
+#TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
+#TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
+#TWILIO_CALLER_ID = os.environ.get('TWILIO_CALLER_ID', None)
+#TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID', None)
